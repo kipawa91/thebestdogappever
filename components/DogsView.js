c59c43865276456367c79c457c972947420dc3e7
Ryan <105310431+murray-dev@users.noreply.github.com>
2023-05-13 21:20:34 +0200

    Add missing key value for React list

--- components/DogsView.js
+++ components/DogsView.js
@@ -3,7 +3,7 @@ import DogCard from './DogCard';
 export default function DogsView({ imageURIs }) {
     return (
         <div className="row">
-            {imageURIs.map(image => <DogCard imageURI={image} />)}
+            {imageURIs.map(image => <DogCard key={image} imageURI={image} />)}
         </div>
     );
 };
